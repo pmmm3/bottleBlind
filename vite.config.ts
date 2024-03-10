b63d505c465612e57d838fa23a7b3722c8a2096c
Pablo Moreno <pablo.triplem@gmail.com>
2024-03-10 19:08:11 +0100

    Add pre-commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,18 @@
-import legacy from '@vitejs/plugin-legacy'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { defineConfig } from 'vite'
+import path from "path";
+import legacy from "@vitejs/plugin-legacy";
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom'
-  }
-})
+	plugins: [vue(), legacy()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	test: {
+		globals: true,
+		environment: "jsdom",
+	},
+});
